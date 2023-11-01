da0c3ce891fb66cc5e12b0fb924d3b697d9b5737
ihaback <ihaback@fastmail.com>
2023-11-01 16:54:51 +0100

    fix logout button styling

--- app/components.tsx
+++ app/components.tsx
@@ -18,7 +18,7 @@ export function Header() {
       <Form action="/logout" method="post">
         <button
           type="submit"
-          className="rounded bg-slate-600 px-4 py-2 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+          className="rounded bg-slate-600 px-4 py-2 text-blue-100 hover:bg-slate-700 disabled:bg-slate-700"
         >
           Logout
         </button>
